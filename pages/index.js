c24d00f345fe69b8d8cd5a3b7de3cbb1b50f9227
Andy <andy@local.com>
2018-11-07 17:35:12 +0000

    First commit

---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import Layout from "../components/Layout.js";
+import React, { Component } from "react";
+import fetch from "isomorphic-unfetch";
+import Link from "next/link";
+import { Config } from "../config.js";
+
+class Index extends Component {
+    static async getInitialProps(context) {
+        const pageRes = await fetch(
+            `${Config.apiUrl}/wp-json/postlight/v1/page?slug=welcome`
+        );
+        const page = await pageRes.json();
+        const postsRes = await fetch(
+            `${Config.apiUrl}/wp-json/wp/v2/posts?_embed`
+        );
+        const posts = await postsRes.json();
+        const pagesRes = await fetch(
+            `${Config.apiUrl}/wp-json/wp/v2/pages?_embed`
+        );
+        const pages = await pagesRes.json();
+        return { page, posts, pages };
+    }
+
+    render() {
+        const posts = this.props.posts.map((post, index) => {
+            return (
+                <ul key={index}>
+                    <li>
+                        <Link
+                            as={`/post/${post.slug}`}
+                            href={`/post?slug=${post.slug}&apiRoute=post`}
+                        >
+                            <a>{post.title.rendered}</a>
+                        </Link>
+                    </li>
+                </ul>
+            );
+        });
+        const pages = this.props.pages.map((page, index) => {
+            return (
+                <ul key={index}>
+                    <li>
+                        <Link
+                            as={`/page/${page.slug}`}
+                            href={`/post?slug=${page.slug}&apiRoute=page`}
+                        >
+                            <a>{page.title.rendered}</a>
+                        </Link>
+                    </li>
+                </ul>
+            );
+        });
+        return (
+            <Layout>
+                <h2>Pages</h2>
+                {pages}
+            </Layout>
+        );
+    }
+}
+
+export default Index;
